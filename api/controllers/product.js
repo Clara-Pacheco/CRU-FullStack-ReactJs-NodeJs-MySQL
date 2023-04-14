4eeb659945066a6824cc5b8c00a8e7a4f3cf6e26
Clara <clarapachecodev@gmail.com>
2023-04-14 17:01:06 -0300

    Import db and export const getProducts passing a request as parameter; inside of this method build an operation to make a query

---
+++ api/controllers/product.js
@@ -0,0 +1,12 @@
+import { db } from "../db.js";
+
+export const getProducts = (_, res) => {
+  const q = "SELECT * FROM produtos";
+
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+
+    return res.status(200).json(data);
+
+  });
+};
