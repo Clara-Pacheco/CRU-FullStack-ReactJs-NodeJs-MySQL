99dfed3dc04501930f3c0422f97a3b8cbe92c58f
Clara <clarapachecodev@gmail.com>
2023-04-30 18:09:39 -0300

    Import Title css component and use it to build App component

--- frontend/crud-supermarket/src/Pages/App.jsx
+++ frontend/crud-supermarket/src/Pages/App.jsx
@@ -1,15 +1,20 @@
-import { Container } from './App.js'
+import { Container, Title } from './App.js'
 
 import { toast, ToastContainer } from 'react-toastify'
 import "react-toastify/dist/ReactToastify.css"
 
+// import { Form } from '../Components/Form.jsx'
+
 export function App() {
   
   return (
+  <>
     <Container>
+      <Title>Produtos</Title>
+    </Container>
     <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
-    <h1>Teste</h1>
-   </Container>
+    
+    </> 
   )
 }
 
