e54239468609349ebaa2895a514d48b8b434b8d0
Clara <clarapachecodev@gmail.com>
2023-04-30 17:50:42 -0300

    Import toast and ToastContainer from react-toastify and use them to build App component

--- frontend/crud-supermarket/src/Pages/App.jsx
+++ frontend/crud-supermarket/src/Pages/App.jsx
@@ -1,11 +1,14 @@
 import { Container } from './App.js'
 
+import { toast, ToastContainer } from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css"
 
 export function App() {
   
   return (
     <Container>
-      <h1>Teste</h1>
+    <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
+    <h1>Teste</h1>
    </Container>
   )
 }
