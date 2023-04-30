b134034ddef6392e3b8f05de0fa35a1f4a2ed022
Clara <clarapachecodev@gmail.com>
2023-04-30 19:31:59 -0300

    Fix color bug in Table css component and create css components Thead and Tr with no stylization and Th with stylization and media querie for mobile devices

--- frontend/crud-supermarket/src/Components/Grid/Grid.js
+++ frontend/crud-supermarket/src/Components/Grid/Grid.js
@@ -2,12 +2,27 @@ import styled from "styled-components";
 
 export const Table=styled.table`
   width: 100%;
-  background-color: ${({ theme}) => theme.COLOR.WHITE};
+  background-color: ${({ theme}) => theme.COLORS.WHITE};
   padding: 20px;
   box-shadow: 0px 0px 5px ${({ theme })=> theme.COLORS.BOX_SHADOW};
   border-radius: 5px;
   max-width: 800px;
   margin: 20px auto;
   word-break: break-all;
+
+`;
+
+export const Thead = styled.thead``;
+
+export const Tr = styled.tr``;
+
+export const Th = styled.th`
+  text-align: start;
+  border-bottom: inset;
+  padding-bottom: 5px;
+
+  @media(max-width: 500px) {
+    ${(props) => props.onlyWeb && "display: none"}
+  }
 
 `;
