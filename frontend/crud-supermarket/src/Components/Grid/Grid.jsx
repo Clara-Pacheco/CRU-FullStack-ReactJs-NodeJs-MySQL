e4bbc28993d246303b9603a488540aa18402b348
Clara <clarapachecodev@gmail.com>
2023-04-30 19:30:06 -0300

    Import Table, Thead, Tr and Th to build Grid component

--- frontend/crud-supermarket/src/Components/Grid/Grid.jsx
+++ frontend/crud-supermarket/src/Components/Grid/Grid.jsx
@@ -1,9 +1,22 @@
-import { Table } from './Grid.js'
+import { Table, Thead, Tr, Th } from './Grid.js'
+
+// import { FaTrash , FaEdit } from "react-icons/fa"
+// import { toast } from "react-toastify"
 
 export function Grid() {
   return (
     <Table>
-      
+      <Thead>
+        <Tr>
+          <Th>Id</Th>
+          <Th>Nome</Th>
+          <Th>Qtd</Th>
+          <Th onlyWeb>Seção</Th>
+          <Th></Th>
+          <Th></Th>
+        </Tr>
+      </Thead>
+
     </Table>
   )
 }
