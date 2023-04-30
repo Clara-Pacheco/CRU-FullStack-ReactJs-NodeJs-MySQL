88343c1ba3f713f9cff497207fda6f9a625e0b75
Clara <clarapachecodev@gmail.com>
2023-04-30 18:35:39 -0300

    Import Label and Input and build the InputArea components inside of App component

--- frontend/crud-supermarket/src/Components/Form.jsx
+++ frontend/crud-supermarket/src/Components/Form.jsx
@@ -1,15 +1,31 @@
 /* eslint-disable react/prop-types */
 import { useRef } from 'react'
 
-import { Container, InputArea } from './Form.js'
+import { Container, InputArea, Label, Input } from './Form.js'
 
-export function Form({ onEdit }){
+export function Form(){
   const ref= useRef()
 
   return (
     <Container ref={ref}>
       <InputArea>
-      
+        <Label>Id do produto</Label>
+        <Input name="id" type='number' />
+      </InputArea>
+
+      <InputArea>
+        <Label>Nome do Produto</Label>
+        <Input name="nome" type='text' />
+      </InputArea>
+
+      <InputArea>
+        <Label>Quantidade em Estoque</Label>
+        <Input name="qtd" type='number' />
+      </InputArea>
+
+      <InputArea>
+        <Label>Seção</Label>
+        <Input name="secao" type='text' />
       </InputArea>
 
 
