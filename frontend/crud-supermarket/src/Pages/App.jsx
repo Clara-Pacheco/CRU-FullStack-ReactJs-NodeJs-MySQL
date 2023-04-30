42368d5f7cebd1825da592e36cb68e60f77859a5
Clara <clarapachecodev@gmail.com>
2023-04-30 19:11:43 -0300

    Import Grid component and use it to build App component

--- frontend/crud-supermarket/src/Pages/App.jsx
+++ frontend/crud-supermarket/src/Pages/App.jsx
@@ -3,7 +3,8 @@ import { Container, Title } from './App.js'
 import { toast, ToastContainer } from 'react-toastify'
 import "react-toastify/dist/ReactToastify.css"
 
-import { Form } from '../Components/Form.jsx'
+import { Form } from '../Components/Form/Form.jsx'
+import { Grid } from '../Components/Grid/Grid.jsx'
 
 export function App() {
   
@@ -12,6 +13,7 @@ export function App() {
     <Container>
       <Title>Produtos</Title>
       <Form />
+      <Grid />
     </Container>
     <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
     
