8d66b92ad200382151902459b6fe82b0272ba2ae
Clara <clarapachecodev@gmail.com>
2023-04-30 18:14:00 -0300

    Import InputArea component and use it to build App component

--- frontend/crud-supermarket/src/Components/Form.jsx
+++ frontend/crud-supermarket/src/Components/Form.jsx
@@ -1,13 +1,16 @@
+/* eslint-disable react/prop-types */
 import { useRef } from 'react'
 
-import { Container } from './Form.js'
+import { Container, InputArea } from './Form.js'
 
-export function Form(){
+export function Form({ onEdit }){
   const ref= useRef()
-  
+
   return (
     <Container ref={ref}>
-
+      <InputArea>
+      
+      </InputArea>
 
 
     </Container>
