fb286667efc1482aeb0606f2ee96b10019cdf8fe
Clara <clarapachecodev@gmail.com>
2023-05-15 18:35:02 -0300

    Build Tbody and Td components

--- frontend/crud-supermarket/src/Components/Grid/Grid.js
+++ frontend/crud-supermarket/src/Components/Grid/Grid.js
@@ -26,3 +26,18 @@ export const Th = styled.th`
   }
 
 `;
+
+export const Tbody = styled.tbody``;
+
+export const Td = styled.td`
+  padding-top: 15px;
+  text-align: ${(props) => (props.alignCenter ? "center" : "start")};
+  width: ${(props) => (props.width ? props.width : "auto" )};
+
+  @media (max-width: 500px) {
+    ${(props) => props.onlyWeb && "display: none"}
+  }
+
+
+
+`;
