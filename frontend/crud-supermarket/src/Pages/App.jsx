8f3dfeb19d40da144c1af6ddca692bb3ccc46928
Clara <clarapachecodev@gmail.com>
2023-06-10 11:53:40 -0300

    Import ThemeProvider, light and dark themes, GlobalStyles, Header componentand create functionality to switch between light and dark modes

--- frontend/crud-supermarket/src/Pages/App.jsx
+++ frontend/crud-supermarket/src/Pages/App.jsx
@@ -3,17 +3,32 @@ import { useState, useEffect } from 'react'
 
 import { Container, Title } from './App.js'
 
+import { ThemeProvider } from 'styled-components'
+import { light } from '../Styles/themes/light.js'
+import dark from '../Styles/themes/dark.js'
+
+
+import GlobalStyles from '../Styles/global.js'
+
 import { toast, ToastContainer } from 'react-toastify'
 import "react-toastify/dist/ReactToastify.css"
 
 import { Form } from '../Components/Form/Form.jsx'
 import { Grid } from '../Components/Grid/Grid.jsx'
+import { Header } from '../Components/Header/Header.jsx'
+
 
 import axios from "axios"
 
 export function App() {
+
   const [products, setProducts] = useState([])
   const [onEdit, setOnEdit] = useState(null)
+  const [theme, setTheme] = useState(light)
+
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light)
+  }
 
   const getProducts = async () => {
     try {
@@ -30,17 +45,19 @@ export function App() {
 
   
   return (
-  <>
+    <>
+    <ThemeProvider theme={theme} >
+    <GlobalStyles />
+      <Container>
+        <Header toggleTheme={toggleTheme} />
+        <Title>Produtos</Title>
+        <Form onEdit={onEdit} setOnEdit={setOnEdit} getProducts={getProducts} />
+        <Grid products={products} setProducts={setProducts} setOnEdit={setOnEdit} />
+      </Container>
+      <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
+    </ThemeProvider>
+      </> 
 
-    <Container>
-      <Title>Produtos</Title>
-      <Form onEdit={onEdit} setOnEdit={setOnEdit} getProducts={getProducts} />
-      <Grid products={products} setProducts={setProducts} setOnEdit={setOnEdit} />
-    </Container>
-
-    <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
-    
-    </> 
   )
 }
 
