93150b96626c1ffe1267fa60c1944b1771d00644
Clara <clarapachecodev@gmail.com>
2023-06-10 11:48:06 -0300

    Delete themes.js

---
+++ frontend/crud-supermarket/src/Styles/themes/dark.js
@@ -0,0 +1,19 @@
+export default {
+
+  title: 'dark',
+  
+  COLORS: {
+    
+    BG:"#222",
+    BOX_SHADOW:"#CCC",
+    INPUT:"#BBB",
+    BTN:"#2c73d2",
+    SWITCHON: "#648DE5",
+    SWITCHOFF: "#CDC392",
+
+    HEADER: "#304C89",
+    H2: "#FFF",
+
+    WHITE:"#FFF",
+  },
+}
---
+++ frontend/crud-supermarket/src/Styles/themes/light.js
@@ -0,0 +1,19 @@
+export const light = {
+
+  title: 'light',
+  
+  COLORS: {
+    TEXT:"#000",
+    BG:"#F2F2F2",
+    BOX_SHADOW:"#CCC",
+    INPUT:"#BBB",
+    BTN:"#2c73d2",
+    SWITCHON: "#648DE5",
+    SWITCHOFF: "#CDC392",
+
+    HEADER: "#304C89",
+    H2: "#000",
+
+    WHITE:"#FFF",
+  },
+}
