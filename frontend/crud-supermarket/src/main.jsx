fc0297895636ebb8157aca45ec919f7a343eed68
Clara <clarapachecodev@gmail.com>
2023-06-10 11:50:23 -0300

    Remove ThemeProvider and GlobalStyles from main.jsx

--- frontend/crud-supermarket/src/main.jsx
+++ frontend/crud-supermarket/src/main.jsx
@@ -1,18 +1,12 @@
+/* eslint-disable no-undef */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './Pages/App.jsx'
 
-import { ThemeProvider } from 'styled-components'
-import theme from './Styles/theme.js'
-import GlobalStyles from './Styles/global.js'
-
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme} >
-    <GlobalStyles />
-       <App />
-    </ThemeProvider>
-  </React.StrictMode>,
+
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+
 )
